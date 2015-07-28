c583aac3fed2a9976d4baa38ccf6f32294e1fd94
Nicholas Lawrance <lawrancn@onid.oregonstate.edu>
2015-07-27 19:21:57 -0700

    First commit

---
+++ scripts/box_waypoints.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import time
+import rospy
+import geodesy.utm
+from geographic_msgs.msg import GeoPose
+# from geographic_msgs.msg import GeoPoint
+from std_msgs.msg import String
+# from geometry_msgs.msg import Quaternion
+
+class WaypointBoxPublisher:
+
+    def __init__(self):
+        print "Creating WaypointBoxPublisher object"
+        rospy.init_node('waypoint_boxer', anonymous=False)
+        self.pos_sub_ = rospy.Subscriber('/crw_geopose_pub', GeoPose, self.pose_callback)
+        self.wp_pub_ = rospy.Publisher('/crw_waypoint_sub', GeoPose, queue_size=10)
+        self.clear_pub_ = rospy.Publisher('/clear_waypoints', String, queue_size=10)
+        
+    def pose_callback(self, msg):
+        # print "Pose message received!"
+        self.cgeopose_ = msg
+        self.cpose_ = msg.position
+        self.cquat_ = msg.orientation        
+        
+    def input_loop(self):
+        out = 'a'
+        while ((not rospy.is_shutdown()) and (out != 'x')):
+            out = raw_input("Enter command: b - make new box, c - clear waypoints, x - exit: ")
+            if (out == 'b'):
+                boxer.send_box()
+            elif (out == 'c'):
+                boxer.send_clear()
+            # rospy.spin()
+        print 'Exiting, goodbye!'
+        
+    def send_box(self):
+        if hasattr(self, 'cgeopose_'):
+            pp = geodesy.utm.fromMsg(self.cpose_)
+            pp.northing += 20.0
+            self.pub_point(pp)
+            pp.easting += 20.0
+            self.pub_point(pp)
+            pp.northing += - 20.0
+            self.pub_point(pp)
+            pp.easting += - 20.0
+            self.pub_point(pp)
+        else:
+            print "No pose found yet, not publishing"                
+            
+    def pub_point(self, pp):
+        self.cpose_ = pp.toMsg()
+        self.cgeopose_.position = self.cpose_
+        self.wp_pub_.publish(self.cgeopose_) 
+        time.sleep(0.2)
+        
+    def send_clear(self):
+        self.clear_pub_.publish("clear")    
+
+if __name__ == '__main__':
+    boxer = WaypointBoxPublisher()
+    try:
+        boxer.input_loop()
+    except KeyboardInterrupt:
+        print "Interrupt encountered, exiting..."
+    except:
+        print "Other exception caught, exiting..."
